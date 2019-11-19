b080acf9f5db4c6fee19aa4813a67e7b3f5d075e
Anna Cotugno <annaeuridice@gmail.com>
2019-11-19 16:40:07 +0000

    post route with form where player names can be entered

--- app.rb
+++ app.rb
@@ -1,19 +1,23 @@
 require 'sinatra/base'
 require 'sinatra'
-
+enable :sessions
 class Battle < Sinatra::Base
 
 get '/' do
   erb(:index)
 end
 
+post '/names' do
+  @p1_name = params[:p1_name]
+  @p2_name = params[:p2_name]
+  erb(:play)
+end
+
 # post '/names' do
-#   @p1_name = params[:p1_name]
+#   session[@p1_name] = params[:p1_name]
 #   @p2_name = params[:p2_name]
 #   erb(:play)
 # end
-
-
 # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,6 +2,10 @@ feature "Testing input from players" do
   scenario 'Display players names' do
     visit('/')
     # for=Player 1 in label
+    #within(:css, '#player_names_form') do
+      #player_names_form should be id attribute
+      #in <form action="/names" method="post",
+      #id='player_names_form'> index.erb
     fill_in "Player 1", :with => "Anna"
     fill_in "Player 2", :with => "George"
     click_button "Submit"
