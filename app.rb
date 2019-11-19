b0fbc0d2d0cde0dfc8325e7d0ac9f2616b3bc4be
Anna Cotugno <annaeuridice@gmail.com>
2019-11-19 17:28:06 +0000

    implents sessions and POST redirect loop

--- app.rb
+++ app.rb
@@ -1,23 +1,29 @@
 require 'sinatra/base'
 require 'sinatra'
-enable :sessions
+# enable :sessions
 class Battle < Sinatra::Base
-
+enable :sessions
 get '/' do
   erb(:index)
 end
 
-post '/names' do
-  @p1_name = params[:p1_name]
-  @p2_name = params[:p2_name]
-  erb(:play)
-end
-
 # post '/names' do
-#   session[@p1_name] = params[:p1_name]
+#   @p1_name = params[:p1_name]
 #   @p2_name = params[:p2_name]
 #   erb(:play)
 # end
+
+post '/names' do
+  session[:p1_name] = params[:p1_name]
+  session[:p2_name] = params[:p2_name]
+  redirect '/play'
+end
+
+get '/play' do
+  @p1_name = session[:p1_name]
+  @p2_name = session[:p2_name]
+  erb(:play)
+end
 # start the server if ruby file executed directly
   run! if app_file == $0
 end
